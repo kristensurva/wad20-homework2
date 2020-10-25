698270aeebe171ff602db82c4b1587218237f83e
Kristen Surva <kristensurva@gmail.com>
2020-10-25 12:49:21 +0200

    Task1 tehtud

--- res/js/script.js
+++ res/js/script.js
@@ -2,6 +2,35 @@
 
 $(function () {
     $('.avatar').click(function() {
-        console.log("test123")
+        if($('.dropdown-content').css("display")==="none") {
+            $('.dropdown-content').css("display", "block")
+            console.log("on")
+        }
+        else {
+            $('.dropdown-content').css("display", "none")
+            console.log("off")
+        }
     });
+
+    function loadUserInfo() {
+        return $.get(
+            {
+                url: 'https://private-anon-09e69e2e84-wad20postit.apiary-mock.com/users/1',
+                success: function (response) {
+                    return response;
+                },
+                error: function () {
+                    alert('error')
+                }
+            }
+        );
+    }
+
+    loadUserInfo().then(function(userinfo) {
+        $('.avatar').attr('src', userinfo.avatar)
+        $('#info').html(userinfo.firstname + " " + userinfo.lastname + "<br>" + userinfo.email)
+    });
+
+
+
 });
